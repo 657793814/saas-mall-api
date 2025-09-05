b806443133dae584bb3703ef503ad91813564eda
刘作栋 <657793814@qq.com>
2025-09-05 11:48:43 +0800

    新增价格计算策略类

--- src/main/java/com/liuzd/soft/service/CalculateStrategies.java
+++ src/main/java/com/liuzd/soft/service/CalculateStrategies.java
@@ -2,8 +2,6 @@ package com.liuzd.soft.service;
 
 import com.liuzd.soft.vo.strategies.CalculateParam;
 
-import java.math.BigDecimal;
-
 /**
  * 计算策略
  *
@@ -13,9 +11,6 @@ import java.math.BigDecimal;
  * @desc
  */
 public interface CalculateStrategies {
-
-    BigDecimal price = null;
-    CalculateParam calculateParam = null;  //计算价格相关数据
 
     void initData(CalculateParam calculateParam);
 
