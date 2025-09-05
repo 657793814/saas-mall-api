171ebbe53887b1d1209b9bbe1e62f90eca5dbec1
刘作栋 <657793814@qq.com>
2025-09-05 10:44:06 +0800

    购物车相关初步提交

---
+++ src/main/java/com/liuzd/soft/service/CalculateStrategies.java
@@ -0,0 +1,18 @@
+package com.liuzd.soft.service;
+
+import java.math.BigDecimal;
+
+/**
+ * @author: liuzd
+ * @date: 2025/8/12
+ * @email: liuzd2025@qq.com
+ * @desc
+ */
+
+public interface CalculateStrategies {
+
+    public BigDecimal price = null;
+
+    public void calculate();
+
+}
---
+++ src/main/java/com/liuzd/soft/service/impl/SecKillStrategies.java
@@ -0,0 +1,23 @@
+package com.liuzd.soft.service.impl;
+
+import com.liuzd.soft.annotation.StrategiesAnnotation;
+import com.liuzd.soft.service.CalculateStrategies;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: liuzd
+ * @date: 2025/9/5
+ * @email: liuzd2025@qq.com
+ * @desc
+ */
+@Service
+@Slf4j
+@StrategiesAnnotation(name = CalculateStrategiesFactory.STRATEGIES_SEC_KILL)
+public class SecKillStrategies implements CalculateStrategies {
+
+    @Override
+    public void calculate() {
+        log.info("策略 {} 计算价格 =============>", CalculateStrategiesFactory.STRATEGIES_GROUP);
+    }
+}
